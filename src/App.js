feee2011515bde32a4e8360c06b374cde01d8093
Adam Azad <adam@adamazad.com>
2020-05-18 01:31:10 +0300

    Styling fixes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     height: 100vh;
-    width: 80vw;
+	width: 100%;
     max-width: 700px;
     margin: 0 auto;
     flex: auto;
@@ -16,19 +16,25 @@ const Container = styled.div`
 
 const Card = styled.div`
     padding: 15px;
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 10px;
+	box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 10px;
+	width:100%;
 `
 
 const Link = styled.a`
     color: #000;
-    margin: 10px 10px;
+	margin: 0px 10px 10px 0px;
     padding: 5px;
 	text-decoration: none;
-	font-weight: 600;
-    
+	font-weight: 600;    
+	display: inline-block;
+
     &:hover {
       text-decoration: underline;
     }
+`
+
+const TextCenter = styled.p`
+	text-align: center;
 `
 
 function App() {
@@ -40,13 +46,13 @@ function App() {
 				<p>He is a freelance web developer and digital entrepreneur with several years of experience in the field. In 2011, at the age of 16, with a business partner from Sweden, he started his first online start-up project.</p>
 				<p>Since 2017, Adam is Affiliate and Project Officer with the Center of Archaeology and Cultural Heritage of his AUIS, a cutting-edge research centre, employing the newest digital technologies to save the cultural heritage of Iraq.</p>
 				<p>An innovative project manager, Adam is currently focusing his researches in the FinTech and decentralized finance (DeFi) ecosystems. His next bid is building products around the ecosystem.</p>
-				<p>
+				<TextCenter>
 					<Link title="Let's work together" href="mailto:adam@adamazad.com">Email</Link>  
 					<Link title="Let's network" href="https://linkedin.com/in/adamazad">LinkedIn</Link> 
 					<Link title="Let's build something awesome" href="https://github.com/adamazad">Github</Link>   
 					<Link title="Tweet me something" href="https://twitter.com/adamzazad">Twitter</Link>   
 					<Link title="Follow me travel blog" href="https://instagram.com/adamazad">Instagram</Link>   
-				</p>
+				</TextCenter>
 			</Card>
       	</Container>
   	);
