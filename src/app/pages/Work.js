6fd9405837728cab85cf12bd844e4318cb9a8684
Adam Azad <adam@adamazad.com>
2020-09-13 23:24:34 +0300

    📱

--- src/app/pages/Work.js
+++ src/app/pages/Work.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { Box, Flex, Link } from 'rebass';
+import Styled from 'styled-components';
+import { Box, Link } from 'rebass/styled-components';
 import Header from '../components/Header';
 import Container from '../styles/Container';
 import HeaderWithContent from '../layouts/HeaderContent';
+
+const WorkFlex = Styled.div(props => `
+  display: flex;
+  min-height: 200px;
+  width: 100%;
+  @media(min-width: ${props.theme.breakpoints[1]}) {
+    &:nth-of-type(odd) {
+      justify-content: flex-start;
+    }
+    &:nth-of-type(even) {
+      justify-content: flex-end;
+    }
+    ${Box} {
+      width: 50%
+    }
+  }
+`);
 
 function Work() {
 
@@ -15,34 +33,30 @@ function Work() {
         <Container fluid width="100%">
           <h1>Work</h1>
           <p>Unless I spend my weekend hiking, I like to explore new technology and work on my projects. Some of which is listed below.</p>
-          <Flex minHeight={ 200 } mx="auto" mb={ 10 } width="100%">
-            <Box width={ 1 / 2 } px={ 2 }>
+          <WorkFlex>
+            <Box>
               <h2>explore.krd</h2>
               <p><Link target="_blank" title="explore.krd" href="https://explore.krd">explore.krd</Link> (all lowercase) is platform for exploring Kurdistan's nature hidden gems. <strong>It is a WIP</strong>. I own <Link target="_blank" title="visit.krd" href="https://visit.krd">visit.krd</Link> if you have an idea for it.</p>
             </Box>
-            <Box width={ 1 / 2 } px={ 2 }></Box>
-          </Flex>
-          <Flex minHeight={ 200 } mx="auto" mb={ 10 } width="100%">
-            <Box width={ 1 / 2 } px={ 2 }></Box>
-            <Box width={ 1 / 2 } px={ 2 }>
+          </WorkFlex>
+          <WorkFlex>
+            <Box>
               <h2>Ethereum Mining</h2>
               <p>I had my first Bitcoin in 2012. In mid-2017, I started mining Ethereum ($ETH) with one <Link target="_blank" href="https://www.amd.com/en/products/graphics/radeon-rx-480">AMD RX 480</Link>. I then scaled it to a fleet of 19 GPUs.</p>
             </Box>
-          </Flex>
-          <Flex minHeight={ 200 } mx="auto" mb={ 10 } width="100%">
-            <Box width={ 1 / 2 } px={ 2 }>
+          </WorkFlex>
+          <WorkFlex>
+            <Box>
               <h2>Deng PWA</h2>
               <p>Deng (Sound in Kurdish) is a PWA for streaming music, heaviliy inspired by Spotify. It was my capstone at AUIS. A WIP version is available at <Link target="_blank" href="https://getdeng.com">getdeng.com</Link></p>
             </Box>
-            <Box width={ 1 / 2 } px={ 2 }></Box>
-          </Flex>
-          <Flex minHeight={ 200 } mx="auto" mb={ 10 } width="100%">
-            <Box width={ 1 / 2 } px={ 2 }></Box>
-            <Box width={ 1 / 2 } px={ 2 }>
+          </WorkFlex>
+          <WorkFlex>
+            <Box>
               <h2>BildX</h2>
               <p>BildX is the self-hosted version of imgix. Contribute on <Link target="_blank" href="https://github.com/adamazad/bildx">GitHub</Link></p>
             </Box>
-          </Flex>
+          </WorkFlex>
         </Container>
       </HeaderWithContent.Content>
     </HeaderWithContent>
