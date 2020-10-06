ce3e99ddafc10288055ca614b7888b5a663d1393
Adam Azad <adam@adamazad.com>
2020-10-06 12:53:55 +0300

    Typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ function Home() {
         <Container fluid width="100%">
           <h1>Adam Azad</h1>
           <h3>Full Stack Engineer, Leader, Speaker, & More</h3>
-          <p>Adam is a Full Stack Engineer and Digital Entrepurenr.</p>
+          <p>Adam is a Full Stack Engineer and Digital Entrepreneur.</p>
           <p>In 2011, at the age of 16, with a business partner from Sweden, he started his first online start-up project.</p>
           <p>He has worked with web development studios and clients in Canada, Europe, South East Asia.</p>
           <p>When he is not working, he likes to educate others about Blockchain, Smart Contracts and Decentralized Finance (DeFi).</p>
