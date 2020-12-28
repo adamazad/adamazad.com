7e1cd5d22ef78e199a5ea2f7da251851069d02ee
Adam Azad <adam@adamazad.com>
2020-12-28 09:03:47 +0300

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import SEO from '../components/seo';
-import Header from '../components/Header';
-import Container from '../styles/Container';
-import HeaderWithContent from '../layouts/HeaderContent';
+import React from 'react'
+import SEO from '../components/seo'
+import Header from '../components/Header'
+import Container from '../styles/Container'
+import HeaderWithContent from '../layouts/HeaderContent'
 
 function Home() {
   return (
@@ -16,14 +16,22 @@ function Home() {
           <h1>Adam Azad</h1>
           <h3>Full Stack Engineer, Leader, Speaker, & More</h3>
           <p>Adam is a Full Stack Engineer and Digital Entrepreneur.</p>
-          <p>In 2011, at the age of 16, with a business partner from Sweden, he started his first online start-up project.</p>
+          <p>
+            In 2011, at the age of 16, with a business partner from Sweden, he started his first online start-up
+            project.
+          </p>
           <p>He has worked with web development studios and clients in Canada, Europe, South East Asia.</p>
-          <p>When he is not working, he likes to educate others about Blockchain, Smart Contracts and Decentralized Finance (DeFi).</p>
-          <p>He is currently residing in <u>Sulaimani</u>.</p>
+          <p>
+            When not working, he likes to educate others about Blockchain, Smart Contracts and Decentralized Finance
+            (DeFi).
+          </p>
+          <p>
+            He is the top contributor from Kurdistan to <strong>JavaScript</strong> on Stack Overflow.
+          </p>
         </Container>
       </HeaderWithContent.Content>
     </HeaderWithContent>
-  );
+  )
 }
 
-export default Home;
+export default Home
