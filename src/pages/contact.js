6fa0b9f4b4cf7aa99e38d152815751a46c07d58e
Adam Azad <adam@adamazad.com>
2021-01-20 08:55:13 +0300

    Add Calendly

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,57 +1,72 @@
-import React from 'react';
-import { Box, Link } from 'rebass';
-import { OutboundLink } from 'gatsby-plugin-google-analytics';
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
+import { Box, Link } from 'rebass'
+import React from 'react'
 
-import SEO from '../components/seo';
-import Header from '../components/Header';
-import Container from '../styles/Container';
-import { HeaderWithContent } from '../layouts';
+import { HeaderWithContent } from '../layouts'
+import Container from '../styles/Container'
+import Header from '../components/Header'
+import SEO from '../components/seo'
 
 function Contact() {
-
-  const links = [{
+  const links = [
+    {
       title: "Let's network",
-      text: "LinkedIn",
-      href: "https://linkedin.com/in/adamazad"
-    },{
-      title: "Tweet me",
-      text: "Twitter",
-      href: "https://twitter.com/in/adamazad"
-    },{
-      title: "GitHub",
-      text: "GitHub",
-      href: "https://github.com/adamazad"
-    },{
-      title: "Stack Overflow",
-      text: "Stack Overflow",
-      href: "https://stackoverflow.com/users/2151050/adam-azad"
-    }, {
-      title: "Instagram",
-      text: "Instagram",
-      href: "https://instagram.com/adamazad"
-    }]
+      text: 'LinkedIn',
+      href: 'https://linkedin.com/in/adamazad',
+    },
+    {
+      title: 'Book A Meeting',
+      text: 'Calendly',
+      href: 'https://calendly.com/adamazad',
+    },
+    {
+      title: 'Send me crypto memes',
+      text: 'Twitter',
+      href: 'https://twitter.com/in/adamazad',
+    },
+    {
+      title: 'Approve my PR!',
+      text: 'GitHub',
+      href: 'https://github.com/adamazad',
+    },
+    {
+      title: 'Stack Overflow',
+      text: 'Stack Overflow',
+      href: 'https://stackoverflow.com/users/2151050/adam-azad',
+    },
+    {
+      title: 'Instagram',
+      text: 'Instagram',
+      href: 'https://instagram.com/adamazad',
+    },
+  ]
 
   return (
     <HeaderWithContent>
       <SEO title="Contact" />
       <HeaderWithContent.Header>
-        <Header/>
+        <Header />
       </HeaderWithContent.Header>
       <HeaderWithContent.Content>
         <Container fluid width="100%">
           <h1>Let's Talk</h1>
-          <p><Link href="mailto:adam@adamazad.com&subject=Hi Adam!">adam@adamazad.com</Link>. Simple and neat, isn't it?</p>
+          <p>
+            <Link href="mailto:adam@adamazad.com&subject=Hi Adam!">adam@adamazad.com</Link>. Simple and neat, isn't it?
+          </p>
           <p>Other places</p>
           <Box>
             <p>
-              { links.map(({ href, title, text }) => <Link target="_blank" as={OutboundLink} mr={2} href={ href } title={ title }>{ text }</Link>) }
+              {links.map(({ href, title, text }) => (
+                <Link target="_blank" as={OutboundLink} mr={2} href={href} title={title}>
+                  {text}
+                </Link>
+              ))}
             </p>
           </Box>
         </Container>
       </HeaderWithContent.Content>
     </HeaderWithContent>
-  );
-
+  )
 }
 
-export default Contact;
+export default Contact
